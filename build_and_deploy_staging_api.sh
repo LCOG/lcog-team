9248a735911a6070305a220bf454ddb0a231bd6c
Sampo Savolainen <ssavolainen@lcog-or.gov>
2025-10-21 21:04:51 -0700

    Add messaging to track progress

--- build_and_deploy_staging_api.sh
+++ build_and_deploy_staging_api.sh
@@ -18,40 +18,49 @@ aws ecr get-login-password --profile $AWS_PROFILE \
 
 # Build and push the container image using Docker CLI
 API_VERSION=$(date -I | tr - .)-$(git rev-parse --short HEAD)
-
-echo "Building API image with version $API_VERSION"
+echo "Building API image with version $API_VERSION ..."
 docker build -t backend/api:$API_VERSION .
+echo "Finished building backend/api:$API_VERSION"
 
+echo "Tagging image with ECR repository URI..."
 docker tag backend/api:$API_VERSION $REPO_URI:$API_VERSION
+echo "Finished tagging image"
 
-echo "Pushing image to ECR repository $REPO_URI"
+echo "Pushing image to ECR repository $REPO_URI ..."
 docker push $REPO_URI:$API_VERSION
+echo "Finished pushing image to ECR at $REPO_URI:$API_VERSION"
 
 # Task definition file
 TDFILE=.aws/lcog-team-staging-backend-task-definition.json
 
 # Check if jq is installed and task definition can be updated automatically
+echo "Checking if jq is installed..."
 if ! command -v jq >/dev/null; then
   echo "\nIt looks like jq is not installed; cannot automatically update the image version in the task definition file $TDFILE.\n\nEdit the task definition file manually and then run the ECS deployment commands manually."
   exit 1
 else
-    echo "Updating the image version in task definition file $TDFILE"
+    echo "Updating the image version in task definition file $TDFILE ..."
   # Update task definition with new image version
     cat <<< "$(jq --arg image "$REPO_URI:$API_VERSION" '.containerDefinitions[0].image = $image' $TDFILE)" > "$TDFILE"
+    echo "Finished updating image version $REPO_URI:$API_VERSION in task definition $TDFILE"
 fi
 
 # Update the task definition
-echo "Registering updated task definition in ECS"
+echo "Registering updated task definition in ECS..."
 aws ecs register-task-definition \
 --cli-input-json "file://./$TDFILE" \
 --no-cli-pager \
 --profile $AWS_PROFILE
+echo "Finished registering updated task definition $TDFILE"
 
 # Update the service deployment to use the latest version of the task definition
-echo "Updating ECS service deployment"
+echo "Starting deployment for ECS service..."
 aws ecs update-service \
 --cluster lcog-team-staging-ecs-cluster \
 --service lcog-team-staging-backend-service \
 --task-definition lcog-team-app-staging-backend-td \
 --no-cli-pager \
 --profile $AWS_PROFILE
+echo "ECS service deployment started"
+
+echo "\n\nGit commit message: Update API image to version $API_VERSION"
